7a794e83fd8511b86300fac9e4a76e5e3ab89332
Mohammed Quadir <86256383+mohdquadir45@users.noreply.github.com>
2024-07-24 18:54:26 +0530

    mongo db connected

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("connected to mongo...."))
+  .catch((error) => console.log(error));
+
+app.listen("3000", () => {
+  console.log("Hello I am server 😁");
+});
